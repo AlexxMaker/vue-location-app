40b964bfee8a5d22c7a74bf953b6db6da6fe95f1
AlexxMaker <alexeus92@mail.ru>
2018-07-30 15:01:41 +0300

    Firebase connected, works bad

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+
+<div>
+ <article v-for="(location, idx) in locations" :key="idx">
+  <img :src="location.image">
+  <h1>{{ location.name }}</h1>
+ </article>
+ <h1> loh </h1>
+ <p> Pidr </p>
+</div>
+
+</template>
+
+<script>
+import { db } from '../main'
+export default {
+ name: 'HelloWorld',
+ data () {
+   return {
+     locations: []
+   }
+ },
+ firestore () {
+   return {
+     locations: db.collection('locations').orderBy('createdAt')
+   }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
