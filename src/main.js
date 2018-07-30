4b724b034538f9decaddfa527d98504fd294538e
Alex Maker <Alexeus92@mail.ru>
2018-07-30 19:13:39 +0300

    Firebase connected, works ok

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+Vue.use(VueFire)
+firebase.initializeApp({
+ projectId: 'vue-test-app-737b6', 
+ databaseURL: 'https://vue-test-app-737b6.firebaseio.com'
+})
+
+export const db = firebase.firestore()
 
 Vue.config.productionTip = false
 
